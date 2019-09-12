e7b7fee1602619c488ad5c95f2200c8b3d17eb09
Shengyu Jin <jin.1024@buckeyemail.osu.edu>
2019-09-12 18:01:54 -0400

    sync

---
+++ Sprint0/Sprint0/IBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    interface IBlock
+    {
+        void Draw();
+        void Update();
+        void Hit(IList<IItem> items, bool isMario, bool isBig);
+    }
+}
