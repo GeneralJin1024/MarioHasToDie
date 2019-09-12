e5520ad194967f622c54f775e88b1661a11e32e4
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2019-09-11 22:36:29 -0400

    Commit changes

--- Sprint0/Sprint0/Sprint0.cs
+++ Sprint0/Sprint0/Sprint0.cs
@@ -19,6 +19,8 @@ namespace Sprint0
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        private String Zhenhao = "HelloWorld";
+
         private ArrayList controllerList;
         private ArrayList spriteList;
         #region Sprite
