a91b834540f82bde38fa571c1197765c91202fc2
Shengyu Jin <jin.1024@osu.edu>
2019-09-12 22:10:52 -0400

    IBlock, IItem added

---
+++ Sprint0/Sprint0/IBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    interface IBlock
+    {
+        void Update();
+        void Draw();
+        void HitRespond(IList<IItem> items, bool isMario, bool isSuperMario);
+    }
+}
