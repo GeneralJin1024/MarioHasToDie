758a341601eb8be3658a699eb945b1f131e4de67
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2019-09-14 13:17:13 -0400

    Delete MarioDied Sprite, MarioDiedCommand Add ActionSprite(Only for mario) Add Mario Class Adding classes are all in MarioClasses folder

---
+++ Sprint0/Sprint0/MarioClasses/Mario.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprint0.MarioClasses
+{
+    class Mario : ISprite
+    {
+        public Texture2D SpriteSheets { get; set; }//useless variable
+
+        //{Stand, Jump, Walk, Crouch}
+        private Texture2D[] StandardMario;
+        private Texture2D[] SuperMario;
+        private Texture2D[] FireMario;
+        private Texture2D DiedSheet;
+
+        private ISprite IdleSprite;
+        private ISprite JumpSprite;
+        private ISprite CrouchSprite;
+        private ISprite WalkingSprite;
+        private ISprite DiedSprite;
+        private ISprite currentMarioAction;
+
+        private bool IsLeft;
+
+        private Vector2 Location;
+
+        public Mario(Texture2D[] standardSheets, Texture2D[] superSheet, Texture2D[] fireSheet, Texture2D diedSheet)
+        {
+            StandardMario = standardSheets;
+            SuperMario = superSheet;
+            FireMario = fireSheet;
+            SetActionSprites();
+            DiedSprite = new ActionSprite(diedSheet, new Point(1, 1));
+            currentMarioAction = IdleSprite;
+            IsLeft = false;
+            Location = new Vector2(400, 300);
+        }
+        #region ISprite Methods
+        public void Update(GameTime gameTime)
+        {
+            currentMarioAction.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, bool isLeft)
+        {
+            currentMarioAction.Draw(spriteBatch, Location, IsLeft);
+        }
+        #endregion ISprite Methods
+
+        private void SetActionSprites()
+        {
+            IdleSprite = new ActionSprite(StandardMario[0], new Point(1, 1));
+            JumpSprite = new ActionSprite(StandardMario[1], new Point(1, 1));
+            WalkingSprite = new ActionSprite(StandardMario[2], new Point(1, 3));
+            CrouchSprite = new ActionSprite(StandardMario[3], new Point(1, 1));
+        }
+    }
+}
