cdbc114a66be585b0bc8636f43c137efa9be5ae7
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2019-09-15 11:55:07 -0400

    Add Factory.cs

---
+++ Sprint0/Sprint0/Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Sprint0.MarioClasses;
+
+namespace Sprint0
+{
+    class Factory
+    {
+        public Mario getMario(Texture2D[] standardSheets, Texture2D[] superSheet,
+            Texture2D[] fireSheet, Texture2D diedSheet, Vector2 location)
+        {
+            return new Mario(standardSheets, superSheet, fireSheet, diedSheet, location);
+        }
+    }
+}
