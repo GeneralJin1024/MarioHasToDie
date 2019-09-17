43802ddafbd79290dbd98813ef39590c7b4a64f4
Runmin Zhou <zhou.2509@buckeyemail.osu.edu>
2019-09-17 19:18:23 -0400

    Enemysprite and Itemsprite

---
+++ Sprint0/Sprint0/Sprites/ItemSprite.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Sprint0.Sprites
+{
+     class ItemSprite : AnimatedSprite
+    {
+        private Vector2 Location;
+        public ItemSprite(Texture2D texture, Vector2 location, Point sheetSize) : base(texture, sheetSize)
+        {
+        Location=location;
+        }
+        public override void Draw(SpriteBatch spriteBatch, Vector2 location, bool isLeft)
+        {
+            base.Draw(spriteBatch, Location, isLeft);
+        }
+    }
+}
