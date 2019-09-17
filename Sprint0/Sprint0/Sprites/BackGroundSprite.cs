3c36173e3bfcbc69603733d5fd925211e06accb2
Runmin Zhou <zhou.2509@buckeyemail.osu.edu>
2019-09-17 15:29:14 -0400

    add background sprite

---
+++ Sprint0/Sprint0/Sprites/BackGroundSprite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.ComponentModel;
+using Sprint0;
+
+namespace sprint0.Enemy
+{
+    class BackgroundSprite : AnimatedPlayerSprite
+    {
+        public BackgroundSprite(Texture2D texture) : base(texture, new Point(1, 2))
+        {
+        }
+        public override void Draw(SpriteBatch spriteBatch, Vector2 Location, bool isLeft)
+        {
+            base.Draw(spriteBatch, new Vector2(600,100), isLeft);
+        }
+    }
+}
