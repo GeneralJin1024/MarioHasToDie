5213462f3188092fee60df71cfea443c61babb11
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2019-09-19 17:41:29 -0400

    Add MarioFactory.cs

--- Sprint0/Sprint0/AbstractFactory.cs
+++ Sprint0/Sprint0/AbstractFactory.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Sprint0.MarioClasses;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics
 
 namespace Sprint0
 {
     public abstract class AbstractFactory
     {
-        public abstract Mario GetMario();
+        public abstract Mario GetMario(Vector2 location);
         public abstract void GetBlock();
         public abstract void GetItem();
         public abstract void GetEnemy();
--- Sprint0/Sprint0/FactoryClasses/MarioFactory.cs
+++ Sprint0/Sprint0/FactoryClasses/MarioFactory.cs
@@ -7,11 +7,18 @@ using Sprint0.MarioClasses;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Sprint0.FactoryClasses
 {
     public class MarioFactory : AbstractFactory
     {
+        private ContentManager Content;
+
+        public MarioFactory(ContentManager content)
+        {
+            Content = content;
+        }
         public override void GetBlock()
         {
         }
@@ -24,10 +31,23 @@ namespace Sprint0.FactoryClasses
         {
         }
 
-        public Mario getMario(Texture2D[] standardSheets, Texture2D[] superSheet,
-            Texture2D[] fireSheet, Vector2 location)
+        public Mario GetMario(Vector2 location)
         {
-            return new Mario(standardSheets, superSheet, fireSheet, location);
+            Texture2D[] StandardSheets = new Texture2D[5] {Content.Load<Texture2D>("MarioSprites/smallMarioRightStand"),
+                Content.Load<Texture2D>("MarioSprites/smallMarioRightJump"),
+                Content.Load<Texture2D>("MarioSprites/smallMarioRightMove"),
+                Content.Load<Texture2D>("MarioSprites/smallMarioRightStand"), Content.Load<Texture2D>("DiedMario/deadMario")};
+            //16*32
+            Texture2D[] SuperSheets = new Texture2D[4] {Content.Load<Texture2D>("SuperMario/superMarioRightStand"),
+                Content.Load<Texture2D>("SuperMario/superMarioJumpRight"),
+                Content.Load<Texture2D>("SuperMario/superMarioMoveRight"),
+                Content.Load<Texture2D>("SuperMario/superMarioRightCrouch")};
+            //16*32   16*22
+            Texture2D[] FireSheets = new Texture2D[4] {Content.Load<Texture2D>("FireMario/fireMarioRightStand"),
+                Content.Load<Texture2D>("FireMario/fireMarioJumpRight"),
+                Content.Load<Texture2D>("FireMario/fireMarioRightMove"),
+                Content.Load<Texture2D>("FireMario/fireMarioRightCrouch")};
+            return new Mario(StandardSheets, SuperSheets, FireSheets, location);
         }
     }
 }
