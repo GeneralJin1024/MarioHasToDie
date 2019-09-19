0764919bc822c859ac2014ded6715346a68f4423
Jian Zhang <zhang.7919@buckeyemail.osu.edu>
2019-09-19 17:55:26 -0400

    Jian Zhang's commit

---
+++ Sprint0/Sprint0/Command/MoveSuperCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sprint0.MarioClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    public class MoveSuperCommand : ICommand
+    {
+        Mario mario;
+        public MoveSuperCommand(Mario mario)
+        {
+            this.mario = mario;
+        }
+
+        public void Execute()
+        {
+            mario.MoveSuper();
+        }
+    }
+}
