2f1397712a3dc2546c1768dd3f6033dcc1f642cf
Zhenhao Lu <lu.1832@buckeyemail.osu.edu>
2019-09-19 17:34:04 -0400

    Add abstract factory

---
+++ Sprint0/Sprint0/AbstractFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint0.MarioClasses;
+
+namespace Sprint0
+{
+    abstract class AbstractFactory
+    {
+        public abstract Mario GetMario();
+        public abstract void GetBlock();
+        public abstract void GetItem();
+        public abstract void GetEnemy();
+    }
+}
---
+++ Sprint0/Sprint0/FactoryClasses/MarioFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint0.MarioClasses;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint0.FactoryClasses
+{
+    public class MarioFactory : AbstractFactory
+    {
+        public override void GetBlock()
+        {
+        }
+
+        public override void GetEnemy()
+        {
+        }
+
+        public override void GetItem()
+        {
+        }
+
+        public Mario getMario(Texture2D[] standardSheets, Texture2D[] superSheet,
+            Texture2D[] fireSheet, Vector2 location)
+        {
+            return new Mario(standardSheets, superSheet, fireSheet, location);
+        }
+    }
+}
