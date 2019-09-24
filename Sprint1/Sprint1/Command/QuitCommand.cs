6b86a4cf56edefa96ba96303db8171325daf3441
Jian Zhang <zhang.7919@buckeyemail.osu.edu>
2019-09-24 00:29:22 -0400

    jian

--- Sprint1/Sprint1/Command/QuitCommand.cs
+++ Sprint1/Sprint1/Command/QuitCommand.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-
-namespace Sprint1
+﻿namespace Sprint1
 {
     public class QuitCommand : ICommand
     {
