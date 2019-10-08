174df6d0df22258c788f6a7649c8b7732dc01aeb
Ziye Zhu <zhu.2044@buckeyemail.osu.edu>
2019-10-08 17:57:30 -0400

    update itemCharacter

--- Sprint1/Sprint1/ItemClasses/CoinCharacter.cs
+++ Sprint1/Sprint1/ItemClasses/CoinCharacter.cs
@@ -7,37 +7,24 @@ namespace Sprint1.ItemClasses
 {
     class CoinCharacter : ItemCharacter
     {
+        public override Sprint1Main.CharacterType Type { get; set; } = Sprint1Main.CharacterType.Coin;
+
         public CoinCharacter(Texture2D texture, Point rowsAndColunms, Vector2 location)
             : base(texture, rowsAndColunms, location)
         {
 
         }
 
-        public override void MarioCollideBottom(MarioCharacter mario)
+        public override Vector2 GetHeightAndWidth()
         {
-            Parameters.IsHidden = true;
-            
-
+            return item.GetHeightAndWidth();
         }
 
-        public override void MarioCollideLeft(MarioCharacter mario)
+        public override void MarioCollide(bool specialCase)
         {
-            Parameters.IsHidden = true;
-          
 
-        }
-
-        public override void MarioCollideRight(MarioCharacter mario)
-        {
-            Parameters.IsHidden = true;
            
-
-        }
-
-        public override void MarioCollideTop(MarioCharacter mario)
-        {
             Parameters.IsHidden = true;
-           
 
         }
     }
--- Sprint1/Sprint1/ItemClasses/PipeCharacter .cs
+++ Sprint1/Sprint1/ItemClasses/PipeCharacter .cs
@@ -12,34 +12,21 @@ namespace Sprint1.ItemClasses
 {
     class PipeCharacter: ItemCharacter
     {
+        public override Sprint1Main.CharacterType Type { get; set; } = Sprint1Main.CharacterType.Pipe;
         public PipeCharacter(Texture2D texture, Point rowsAndColunms, Vector2 location)
             : base(texture, rowsAndColunms, location)
         {
 
         }
 
-        public override void MarioCollideBottom(MarioCharacter mario)
+        public override Vector2 GetHeightAndWidth()
+        {
+            return item.GetHeightAndWidth();
+        }
+
+        public override void MarioCollide(bool specialCase)
         {
           
-
-        }
-
-        public override void MarioCollideLeft(MarioCharacter mario)
-        {
-           
-
-        }
-
-        public override void MarioCollideRight(MarioCharacter mario)
-        {
-           
-
-        }
-
-        public override void MarioCollideTop(MarioCharacter mario)
-        {
-           
-
         }
     }
 }
