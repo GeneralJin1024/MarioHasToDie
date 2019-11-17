3c9304e6b16c525c054ac7586df337ee6a01f0aa
Ziye Zhu <zhu.2044@buckeyemail.osu.edu>
2019-11-17 11:07:50 -0500

    update implementation jumpMedicine RandomItem BombCharacter

--- Sprint1/Sprint1/ItemEnemyClasses/JumpMedicineCharacter.cs
+++ Sprint1/Sprint1/ItemEnemyClasses/JumpMedicineCharacter.cs
@@ -9,7 +9,33 @@ using System.Threading.Tasks;
 
 namespace Sprint1.ItemClasses
 {
-    class JumpMedicineCharacter// : ItemCharacter
+    class JumpMedicineCharacter : ItemCharacter
     {
+        public override Vector2 GetHeightAndWidth { get { return Item.GetHeightAndWidth; } }
+        public override Sprint1Main.CharacterType Type { get; set; } = Sprint1Main.CharacterType.JumpMedicine;
+        public JumpMedicineCharacter(Texture2D texture, Point rowsAndColunms, Vector2 location)
+            : base(texture, rowsAndColunms, location)
+        {
+
+        }
+
+        public override void Update(float timeOfFrame)
+        {
+            base.Update(timeOfFrame);
+        }
+
+        //public override Vector2 GetHeightAndWidth()
+        //{
+        //    return Item.GetHeightAndWidth;
+        //}
+
+        public override void MarioCollide(bool specialCase)
+        {
+
+           
+            Parameters.IsHidden = true;
+            SoundFactory.Instance.MarioGetItem();
+
+        }
     }
 }
--- Sprint1/Sprint1/ItemEnemyClasses/RandomItemCharacter.cs
+++ Sprint1/Sprint1/ItemEnemyClasses/RandomItemCharacter.cs
@@ -8,8 +8,51 @@ using System.Threading.Tasks;
 
 namespace Sprint1.ItemClasses
 {
-    class RandomItemCharacter// : ItemCharacter
+    class RandomItemCharacter : ItemCharacter
     {
-        
+        public override Vector2 GetHeightAndWidth { get { return Item.GetHeightAndWidth; } }
+        public override Sprint1Main.CharacterType Type
+        {
+            get { return TypeList[CurrentType]; }
+            set { }
+        }
+        private Sprint1Main.CharacterType[] TypeList;
+        private int CurrentType;
+        private float Time;
+        public RandomItemCharacter(Texture2D texture, Point rowsAndColunms, Vector2 location)
+            : base(texture, rowsAndColunms, location)
+        {
+            CurrentType = 0;
+            Time = 0;
+            TypeList = new Sprint1Main.CharacterType[] { Sprint1Main.CharacterType.RedMushroom,
+                Sprint1Main.CharacterType.Star, Sprint1Main.CharacterType.GreenMushroom,
+                Sprint1Main.CharacterType.Flower, Sprint1Main.CharacterType.Bomb};
+        }
+
+        public override void Update(float timeOfFrame)
+        {
+          // Time += timeOfFrame;
+            base.Update(timeOfFrame);
+
+            Time += timeOfFrame;
+            if(Time >= 1)
+            {
+                Time -= 1;
+                CurrentType++;
+                CurrentType = CurrentType >= TypeList.Length ? 0 : CurrentType;
+            }
+           
+        }
+
+       
+
+        public override void MarioCollide(bool specialCase)
+        {
+
+            Parameters.IsHidden = true;
+            if(Type != Sprint1Main.CharacterType.Bomb)
+                SoundFactory.Instance.MarioGetItem();
+
+        }
     }
 }
