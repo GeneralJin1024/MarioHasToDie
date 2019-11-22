66526b2bf224446d907fd9fcb684017442c6881c
Jian Zhang <zhang.7919@buckeyemail.osu.edu>
2019-11-21 23:50:30 -0500

    Mario State Store buy super state

---
+++ Sprint1/Sprint1/Command/BuySuperStateCommand.cs
@@ -0,0 +1,23 @@
+﻿using Sprint1.MarioClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1
+{   // Command Class for the Avatar to Super state
+    public class BuySuperStateCommand : ICommand
+    {
+        MarioCharacter mario;
+        public BuySuperStateCommand(MarioCharacter mario)
+        {
+            this.mario = mario;
+        }
+
+        public void Execute()
+        {   // take the receiver method of power state change
+            mario.MoveSuper();
+        }
+    }
+}
