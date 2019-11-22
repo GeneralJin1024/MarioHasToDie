9e2e98f8efe34bc23448e0cc23da35bbec035992
Jian Zhang <zhang.7919@buckeyemail.osu.edu>
2019-11-22 00:19:27 -0500

    State Store Update to Fire and Star State

---
+++ Sprint1/Sprint1/Command/BuyExtralifeCommand.cs
@@ -0,0 +1,24 @@
+﻿using Sprint1.MarioClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1
+{   // Command Class for the Avatar to fire state
+    public class BuyExtralifeCommand : ICommand
+    {
+        MarioCharacter mario;
+        public BuyExtralifeCommand(MarioCharacter mario)
+        {
+            this.mario = mario;
+        }
+
+        public void Execute()
+        {   // The Mario get one more extra life
+            Sprint1Main.MarioLife++;
+        }
+    }
+}
+
---
+++ Sprint1/Sprint1/Command/BuyFireStateCommand.cs
@@ -0,0 +1,23 @@
+﻿using Sprint1.MarioClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1
+{   // Command Class for the Avatar to fire state
+    public class BuyFireStateCommand : ICommand
+    {
+        MarioCharacter mario;
+        public BuyFireStateCommand(MarioCharacter mario)
+        {
+            this.mario = mario;
+        }
+
+        public void Execute()
+        {   // take the receiver method of power state change
+            mario.MoveFire();
+        }
+    }
+}
